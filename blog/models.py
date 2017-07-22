892c04e093c07247659c48da784396c536b7d66a
wp121212 <139094187@qq.com>
2017-07-22 15:00:13 +0800

    添加数据库表的模型

--- blog/models.py
+++ blog/models.py
@@ -5,7 +5,10 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 # 用户模型
-class User(AbstractUser):
+class User(models.Model):
+
+
+
     #     #null：
     #     If True, Django will store empty values as NULL in the database. Default
     # is False.
@@ -20,6 +23,15 @@ class User(AbstractUser):
     # unique  =true 不能出现相同的内容，否则会出异常
     mobile = models.CharField(max_length=11, blank=True, null=True, unique=True, verbose_name='电话号码')
 
+    username = models.CharField(max_length=20,blank=True,null=True,verbose_name='用户名')
+
+    password = models.CharField(max_length=20,blank=True,null=True,verbose_name='密码')
+
+    email = models.EmailField(verbose_name='邮箱',blank=True,null=True)
+
+    date_publish = models.DateTimeField(auto_now_add=True,blank=True,null=True,verbose_name='注册的时间')
+
+    last_name = models.CharField(verbose_name='last_name', max_length=30, blank=True)
     class Meta:
         # verbose_name的意思很简单，就是给你的模型类起一个更可读的名字：
         # verbose_name = "pizza"
@@ -48,7 +60,7 @@ class Tag(models.Model):
 
         verbose_name_plural = verbose_name
 
-        ordering = [id]
+        ordering = ['id']
 
     def __str__(self):
         return self.name
@@ -82,5 +94,97 @@ class Article(models.Model):
 
     is_recommend = models.BooleanField(default=True, verbose_name='是否推荐')
 
-    date_publish = models.DateTimeField(auto_now_add=True, verbose_name='发布时间'
-                                        )
+    date_publish = models.DateTimeField(auto_now_add=True, verbose_name='发布时间')
+
+    user = models.ForeignKey(User,verbose_name='用户')
+
+    category = models.ForeignKey(Category,verbose_name='分类',blank=True,null=True)
+
+    tag = models.ManyToManyField(Tag,verbose_name='标签')
+
+    class Meta:
+        verbose_name = '文章'
+        verbose_name_plural = verbose_name
+
+        ordering=['-date_publish']
+
+    def __str__(self):
+        return self.title
+
+#评论模型
+
+class Comment(models.Model):
+
+    content = models.TextField(verbose_name='评论内容')
+
+    date_publish = models.DateTimeField(auto_now_add=True,blank=True,null=True,verbose_name='评论时间')
+
+    user = models.ForeignKey(User,blank=True,verbose_name='用户',null=True)
+
+    article = models.ForeignKey(Article,blank=True,null=True,verbose_name='文章')
+
+    pid = models.ForeignKey('self',blank=True,null=True,verbose_name='父级评论')
+
+    class Meta:
+        verbose_name = '评论'
+
+        verbose_name_plural = verbose_name
+
+        ordering = ['-date_publish']
+
+    def  __str__(self):
+        return  self.id
+class Links(models.Model):
+
+    title = models.CharField(max_length=50,verbose_name='标题')
+
+    description = models.CharField(max_length=200,verbose_name='友情链接描述')
+
+    callback_url = models.URLField(verbose_name='url地址')
+
+    date_publish = models.DateTimeField(auto_now_add=True,blank=True,null=True,verbose_name='发布时间')
+
+    index =models.IntegerField(default=999,verbose_name='排列顺序（从小到大）')
+
+    class Meta:
+
+        verbose_name = '友情链接'
+
+        verbose_name_plural = verbose_name
+
+        ordering = ['index','id']
+
+    def __str__(self):
+         return self.title
+
+class Ad(models.Model):
+
+    title = models.CharField(max_length=50,verbose_name='广告标题')
+
+    description = models.CharField(max_length=200,verbose_name='广告描述')
+
+    imgage_url = models.ImageField(upload_to='ad/%Y/%m',verbose_name='图片路径')
+
+    callback_url = models.URLField(null=True,blank=True,verbose_name='回调url')
+
+    date_publish = models.DateTimeField(auto_now_add=True,verbose_name='发布时间')
+
+    index = models.IntegerField(default=999,verbose_name='排序顺序（从小到大）')
+
+    class Meta:
+        verbose_name = u'广告'
+
+        verbose_name_plural = verbose_name
+
+        ordering = ['index','id']
+
+    def __str__(self):
+        return  self.title
+
+
+
+
+
+
+
+
